d2297d2b498b41cc8a8f9529bb574c8b4d925fce
Gabriel Baril <gabriel-baril2010@hotmail.com>
2024-12-14 14:03:01 -0500

    Update glfw.sharpmake.cs

--- glfw.sharpmake.cs
+++ glfw.sharpmake.cs
@@ -9,6 +9,19 @@ public class GLFWProject : BaseCppProject
         Name = "glfw";
         SourceRootPath = @"[project.SharpmakeCsPath]\src";
         AddTargets(TargetUtil.DefaultTarget);
+
+        SourceFilesExtensions.Clear();
+
+        SourceFiles.Add(new string[]
+        {
+            "glfw_config.h",
+            "context.c",
+            "init.c",
+            "input.c",
+            "monitor.c",
+            "vulkan.c",
+            "window.c"
+        });
     }
 
     [Configure]
@@ -23,7 +36,41 @@ public class GLFWProject : BaseCppProject
         conf.IntermediatePath = @"[project.SharpmakeCsPath]\Out\Intermediate\[target.Platform]-[target.Optimization]";
         conf.IncludePaths.Add(@"[project.SharpmakeCsPath]\include");
 
-        conf.Defines.Add("_GLFW_WIN32");
-        conf.Defines.Add("_CRT_SECURE_NO_WARNINGS");
+        if(target.Platform == Platform.win32 || target.Platform == Platform.win64)
+        {
+            SourceFiles.Add(new string[]
+            {
+                "win32_init.c",
+                "win32_joystick.c",
+                "win32_monitor.c",
+                "win32_time.c",
+                "win32_thread.c",
+                "win32_window.c",
+                "wgl_context.c",
+                "egl_context.c",
+                "osmesa_context.c"
+            });
+
+            conf.Defines.Add("_GLFW_WIN32");
+            conf.Defines.Add("_CRT_SECURE_NO_WARNINGS");
+        }
+        else if(target.Platform == Platform.linux)
+        {
+            SourceFiles.Add(new string[]
+            {
+                "src/x11_init.c",
+                "src/x11_monitor.c",
+                "src/x11_window.c",
+                "src/xkb_unicode.c",
+                "src/posix_time.c",
+                "src/posix_thread.c",
+                "src/glx_context.c",
+                "src/egl_context.c",
+                "src/osmesa_context.c",
+                "src/linux_joystick.c"
+            });
+
+            conf.Defines.Add("_GLFW_X11");
+        }
     }
 }
