7867e6b6e4f099ff2c358f1339d9a5e57aa70d8f
Gabriel Baril <gabriel-baril2010@hotmail.com>
2024-12-14 14:43:41 -0500

    Updated gitignore and sharpmake file to make the dependent code compile

--- glfw.sharpmake.cs
+++ glfw.sharpmake.cs
@@ -9,19 +9,6 @@ public class GLFWProject : BaseCppProject
         Name = "glfw";
         SourceRootPath = @"[project.SharpmakeCsPath]\src";
         AddTargets(TargetUtil.DefaultTarget);
-
-        SourceFilesExtensions.Clear();
-
-        SourceFiles.Add(new string[]
-        {
-            "glfw_config.h",
-            "context.c",
-            "init.c",
-            "input.c",
-            "monitor.c",
-            "vulkan.c",
-            "window.c"
-        });
     }
 
     [Configure]
@@ -38,37 +25,22 @@ public class GLFWProject : BaseCppProject
 
         if(target.Platform == Platform.win32 || target.Platform == Platform.win64)
         {
-            SourceFiles.Add(new string[]
-            {
-                "win32_init.c",
-                "win32_joystick.c",
-                "win32_monitor.c",
-                "win32_time.c",
-                "win32_thread.c",
-                "win32_window.c",
-                "wgl_context.c",
-                "egl_context.c",
-                "osmesa_context.c"
-            });
+            conf.SourceFilesBuildExclude.Add("x11_init.c");
+            conf.SourceFilesBuildExclude.Add("x11_monitor.c");
+            conf.SourceFilesBuildExclude.Add("x11_window.c");
+            conf.SourceFilesBuildExclude.Add("linux_joystick.c");
 
             conf.Defines.Add("_GLFW_WIN32");
             conf.Defines.Add("_CRT_SECURE_NO_WARNINGS");
         }
         else if(target.Platform == Platform.linux)
         {
-            SourceFiles.Add(new string[]
-            {
-                "src/x11_init.c",
-                "src/x11_monitor.c",
-                "src/x11_window.c",
-                "src/xkb_unicode.c",
-                "src/posix_time.c",
-                "src/posix_thread.c",
-                "src/glx_context.c",
-                "src/egl_context.c",
-                "src/osmesa_context.c",
-                "src/linux_joystick.c"
-            });
+            conf.SourceFilesBuildExclude.Add("win32_init.c");
+            conf.SourceFilesBuildExclude.Add("win32_joystick.c");
+            conf.SourceFilesBuildExclude.Add("win32_monitor.c");
+            conf.SourceFilesBuildExclude.Add("win32_time.c");
+            conf.SourceFilesBuildExclude.Add("win32_thread.c");
+            conf.SourceFilesBuildExclude.Add("win32_window.c");
 
             conf.Defines.Add("_GLFW_X11");
         }
