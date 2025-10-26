7a1cfd7ea7cd390234ebf8106600fdc9dd17ce81
Gabriel Baril <gabriel-baril2010@hotmail.com>
2025-10-25 23:47:07 -0400

    Update glfw.sharpmake.cs

--- glfw.sharpmake.cs
+++ glfw.sharpmake.cs
@@ -2,11 +2,11 @@ using System.IO; // For Path.Combine
 using Sharpmake; // Contains the entire Sharpmake object library.
 
 [Generate]
-public class GLFWProject : BaseCppProject
+public class HdnCodeExternalGLFWProject : BaseCppProject
 {
-    public GLFWProject()
+    public HdnCodeExternalGLFWProject()
     {
-        Name = "glfw";
+        Name = "hdn.code.external.glfw";
         SourceRootPath = @"[project.SharpmakeCsPath]\src";
         AddTargets(TargetUtil.DefaultTarget);
     }
